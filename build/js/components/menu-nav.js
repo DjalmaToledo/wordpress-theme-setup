67b6db1578e124d85872a06366e0282c3cc64f20
Djalma Toledo <djalmabt@gmail.com>
2020-05-18 10:06:38 -0300

    adicionando novas seções

--- build/js/components/menu-nav.js
+++ build/js/components/menu-nav.js
@@ -1,43 +1,8 @@
 $(document).ready(function(){
 
-	if($(window).width() > 1024) {
+	$("body").on("click", '.nav-item', function(e) {
+		$('.nav-item').toggleClass("open");
+		$('.rmm').toggleClass("open");
+	});
 
-		$(".menu li .sub-menu li .icon-submenu").on("click", function(e) {
-			e.preventDefault();
-			$(this).siblings().next(".sub-menu").toggleClass("sub-menu-show");
-			$(this).toggleClass("active");
-		});
-
-
-
-		//$(".menu-item-has-children").prepend("<span class='icon-submenu'></span>");
-		//$(".menu-item-type-custom > .icon-submenu").click(false);
-
-	};
-
-	$('.nav-item').append("<div class='tap-mobile'></div>");
-
-	if($(window).width() < 1024) {
-
-			var a = !1;
-			$("body").on("click", '.nav-item', function(e) {
-				$('.nav-item').toggleClass("open");
-				0 == a ? ($("header .rmm").css({
-					left: "0",
-					opacity: "1"
-				}), a = !0) : ($("header .rmm").css({
-					opacity: "2px",
-					left: "-230px"
-				}), a = !1), e.preventDefault()
-			});
-
-		$(".menu-item-has-children").prepend("<span class='icon-submenu'></span>");
-		$(".menu-item-type-custom > .icon-submenu").click();
-		$(".icon-submenu").on("click", function(e) {
-			e.preventDefault();
-			$(this).siblings().next(".sub-menu").toggleClass("sub-menu-show-mobile");
-			$(this).toggleClass("active");
-		});
-
-	}
 });
--- footer.php
+++ footer.php
@@ -4,22 +4,23 @@
  *
  * @package SOMA Dev
  * @since 0.0.1
- */
-wp_footer(); ?>
-<?php if ( ! dynamic_sidebar( 'whatsapp' ) ) : endif; ?>
-		<!-- <span class="top-button"></span> -->
-		<footer class="footer">
-			<div class="wrapper-footer">
+ */ ?>
+		<footer id="footer">
+			<section class="wrapper-footer">
 				<div class="center">
-					<?php if ( ! dynamic_sidebar( 'footer1' ) ) : endif; ?>
-					<?php if ( ! dynamic_sidebar( 'footer2' ) ) : endif; ?>
-					<?php if ( ! dynamic_sidebar( 'footer3' ) ) : endif; ?>
-					<?php if ( ! dynamic_sidebar( 'footer4' ) ) : endif; ?>
+					<h3>Rodapé do Site</h3>
+					<div class="col col-4"><?php if ( ! dynamic_sidebar( 'footer1' ) ) : endif; ?></div
+					><div class="col col-4"><?php if ( ! dynamic_sidebar( 'footer2' ) ) : endif; ?></div
+					><div class="col col-4"><?php if ( ! dynamic_sidebar( 'footer3' ) ) : endif; ?></div
+					><div class="col col-4"><?php if ( ! dynamic_sidebar( 'footer4' ) ) : endif; ?></div>
 				</div>
-			</div>
-			<div class="copyright">
-				<span>Copyright Pizzani 2020 - Todos os direitos reservados</span>
-			</div>
+			</section>
+			<section class="copyright textcenter">
+				<div class="center">
+					<p>Copyright Pizzani 2020 - Todos os direitos reservados</p>
+				</div>
+			</section>
 		</footer>
+		<?php wp_footer(); ?>
 	</body>
 </html>
